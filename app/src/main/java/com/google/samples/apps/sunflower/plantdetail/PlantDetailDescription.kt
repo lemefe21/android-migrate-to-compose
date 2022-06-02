a0842414826772b8b5bb4a76e1cdff59c85368e9
Felipe Leme <lemefe21@gmail.com>
2022-06-02 11:00:17 -0300

    [MAIN] migrate plan name view to composable

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -17,17 +17,36 @@
 package com.google.samples.apps.sunflower.plantdetail
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
 
 @Composable
 fun PlantDetailDescription() {
     Surface {
         Text("Hello Compose")
     }
+}
+
+@Composable
+fun PlanName(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
 
 @Preview(
@@ -42,5 +61,20 @@ fun PlantDetailDescription() {
 fun PreviewPlantDetailDescription() {
     MaterialTheme {
         PlantDetailDescription()
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewPlantName() {
+    MaterialTheme {
+        PlanName("Apple")
     }
 }
