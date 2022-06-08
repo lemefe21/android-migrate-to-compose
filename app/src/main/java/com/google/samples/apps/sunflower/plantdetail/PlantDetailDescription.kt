7a4d1737e75587e194be2b593d8df519c21fc4b3
Felipe Leme <lemefe21@gmail.com>
2022-06-08 16:19:48 -0300

    [MAIN] pass viewmodel to composable

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -21,24 +21,34 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
+fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
+    val plant by plantDetailViewModel.plant.observeAsState()
+
+    plant?.let {
+        PlantDetailContent(it)
     }
 }
 
 @Composable
-fun PlanName(name: String) {
+fun PlantDetailContent(plant: Plant) {
+    PlantName(name = plant.name)
+}
+
+@Composable
+fun PlantName(name: String) {
     Text(
         text = name,
         style = MaterialTheme.typography.h5,
@@ -60,7 +70,30 @@ fun PlanName(name: String) {
 @Composable
 fun PreviewPlantDetailDescription() {
     MaterialTheme {
-        PlantDetailDescription()
+        // PlantDetailDescription()
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewPlantDetailContent() {
+    val plant = Plant(
+        plantId = "1111",
+        name = "Apple",
+        description = "Descriptions",
+        growZoneNumber = 3,
+        wateringInterval = 30,
+        imageUrl = "")
+
+    MaterialTheme {
+        PlantDetailContent(plant)
     }
 }
 
@@ -75,6 +108,6 @@ fun PreviewPlantDetailDescription() {
 @Composable
 fun PreviewPlantName() {
     MaterialTheme {
-        PlanName("Apple")
+        PlantName("Apple")
     }
 }
