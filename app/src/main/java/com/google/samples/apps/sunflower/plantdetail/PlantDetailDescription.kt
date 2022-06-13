18433cc4f67283d3312b80ef9219547b7e13d50a
Felipe Leme <lemefe21@gmail.com>
2022-06-13 14:39:24 -0300

    [MAIN] change to use interop theming

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -130,7 +131,7 @@ private fun PlantDescription(description: String) {
 )
 @Composable
 fun PreviewPlantDetailDescription() {
-    MaterialTheme {
+    MdcTheme {
         // PlantDetailDescription()
     }
 }
@@ -153,7 +154,7 @@ fun PreviewPlantDetailContent() {
         wateringInterval = 30,
         imageUrl = "")
 
-    MaterialTheme {
+    MdcTheme {
         PlantDetailContent(plant)
     }
 }
@@ -168,7 +169,7 @@ fun PreviewPlantDetailContent() {
 )
 @Composable
 fun PreviewPlantName() {
-    MaterialTheme {
+    MdcTheme {
         PlantName("Apple")
     }
 }
@@ -183,7 +184,7 @@ fun PreviewPlantName() {
 )
 @Composable
 fun PreviewPlantWatering() {
-    MaterialTheme {
+    MdcTheme {
         PlantWatering(2)
     }
 }
@@ -198,7 +199,7 @@ fun PreviewPlantWatering() {
 )
 @Composable
 fun PreviewPlantDescription() {
-    MaterialTheme {
+    MdcTheme {
         PlantDescription("HTML<br><br>description")
     }
 }
